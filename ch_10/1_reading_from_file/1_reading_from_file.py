d9976f92efa8587d4137d0553465a9bf573510e9
v-iushin <yvo.yushin@gmail.com>
2026-05-23 23:58:43 +0300

    PCC: chapter 10, reading from file, extended

--- ch_10/1_reading_from_file/1_reading_from_file.py
+++ ch_10/1_reading_from_file/1_reading_from_file.py
@@ -1,5 +1,7 @@
 # READING FROM FILE
 
+#! python interprets all text in file as string
+
 '''
 from pathlib import Path
 BASE = Path(__file__).parent
@@ -10,10 +12,34 @@ print(text)
 '''
 
 from pathlib import Path
+
 BASE = Path(__file__).parent
 path = Path(BASE/"pi_digits.txt")
 #path = Path("pcc_notes/ch_10/1_reading_from_file/pi_digits.txt")
+
 contents = path.read_text()#.rstrip()
 print(contents)
 print()
 
+# splitlines() turns long string into 
+# set of lines
+# return list of all lines
+lines = contents.splitlines()
+for line in lines:
+    print(line)
+print()
+
+pi_string = ""
+for line in lines:
+    pi_string += line.lstrip()
+print(pi_string)
+print(len(pi_string))
+print()
+
+
+
+# LARGE FILES
+
+path = Path(BASE/"pi_million_digits.txt")
+
+contents = path.read_text()
