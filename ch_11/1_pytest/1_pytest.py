1b46f81ef35bee4cc8f526efbd033f0f6262788d
v-iushin <yvo.yushin@gmail.com>
2026-06-01 23:59:30 +0300

    PCC: chapter 11, pytest

---
+++ ch_11/1_pytest/1_pytest.py
@@ -0,0 +1,24 @@
+# PYTEST
+
+# to update
+# python -m pip install --upgrade pip
+# python -m pip install --upgrade package_name
+
+# to install
+# python -m pip install --user pytest
+# python -m pip install --user package_name
+
+from name_function import get_formatted_name
+
+print("Enter 'q' at any time to quit.")
+while True:
+    first = input("\nPlease give me a first name: ")
+    if first == "q":
+        break
+    last = input("Please give me a last name: ")
+    if last == "q":
+        break
+    
+    formatted_name = get_formatted_name(first, last)
+    print(f"\tNeatly formatted name: {formatted_name}.")
+
