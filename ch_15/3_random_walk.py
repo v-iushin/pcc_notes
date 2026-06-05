425b59d6e629ade84d50278489cd3d41d44482bf
v-iushin <yvo.yushin@gmail.com>
2026-06-05 23:58:10 +0300

    PCC: chapter 15, random walk

---
+++ ch_15/3_random_walk.py
@@ -0,0 +1,60 @@
+# RANDOM WALK
+
+from random import choice
+
+class RandomWalk:
+    """A class to generate random walks."""
+    
+    def __init__(self, num_points=5000):
+        """Initialize attributes of a walk."""
+        self.num_points = num_points
+        # All walks start at (0, 0).
+        self.x_values = [0]
+        self.y_values = [0]
+    
+    def fill_walk(self):
+        """Calculate all points in hte walk."""
+        # Keep taking steps until the walk reaches the desired length.
+        while len(self.x_values) < self.num_points:
+            # Decide which direction to go, and how far to go.
+            x_direction = choice([1, -1])
+            x_distance = choice([0, 1, 2, 3, 4])
+            x_step = x_direction * x_distance
+            y_direction = choice([1, -1])
+            y_distance = choice([0, 1, 2, 3, 4])
+            y_step = y_direction * y_distance
+            # Reject moves that go nowhere.
+            if x_step == 0 and y_step == 0:
+                continue
+            # Calculate the new position.
+            x = self.x_values[-1] + x_step
+            y = self.y_values[-1] + y_step
+            self.x_values.append(x)
+            self.y_values.append(y)
+
+import matplotlib.pyplot as plt
+
+# Keep making new walks, as long as the program is active.
+while True:
+    # Make a random walk.
+    rw = RandomWalk()
+    rw.fill_walk()
+
+    # Plot the points in the walk.
+    plt.style.use("classic")
+    fig, ax = plt.subplots()
+    point_numbers = range(rw.num_points)
+    #! ax.scatter(rw.x_values, rw.y_values, s=10)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors="none", s=10)
+    # equal scaling of both axes
+    ax.set_aspect("equal")
+    
+    # Emphasize the first and last points.
+    ax.scatter(0, 0, c="green", edgecolors="none", s=50)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c="red", edgecolors="none", s=50)
+    
+    plt.show()
+    
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == "n":
+        break
