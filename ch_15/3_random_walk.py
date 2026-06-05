a43b9b417adea3df839962b0e23c8a1df720f82c
v-iushin <yvo.yushin@gmail.com>
2026-06-06 00:21:21 +0300

    PCC: chapter 15, random walk, extended

--- ch_15/3_random_walk.py
+++ ch_15/3_random_walk.py
@@ -37,24 +37,30 @@ import matplotlib.pyplot as plt
 # Keep making new walks, as long as the program is active.
 while True:
     # Make a random walk.
-    rw = RandomWalk()
+    rw = RandomWalk(50_000)
     rw.fill_walk()
 
     # Plot the points in the walk.
     plt.style.use("classic")
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(15,9))
     point_numbers = range(rw.num_points)
     #! ax.scatter(rw.x_values, rw.y_values, s=10)
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors="none", s=10)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors="none", s=1)
     # equal scaling of both axes
     ax.set_aspect("equal")
     
     # Emphasize the first and last points.
-    ax.scatter(0, 0, c="green", edgecolors="none", s=50)
-    ax.scatter(rw.x_values[-1], rw.y_values[-1], c="red", edgecolors="none", s=50)
+    ax.scatter(0, 0, c="green", edgecolors="none", s=15)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c="red", edgecolors="none", s=15)
+    
+    # Remove the axes.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
     
     plt.show()
     
     keep_running = input("Make another walk? (y/n): ")
     if keep_running == "n":
         break
+
+# pdf 355
