396aa043bb1c33111a1865734fe20a6a66ffcc25
v-iushin <yvo.yushin@gmail.com>
2026-06-11 20:56:42 +0300

    PCC: chapter 16, geojson format

---
+++ ch_16/4_geojson_format.py
@@ -0,0 +1,17 @@
+# GEOJSON FORMAT
+
+from pathlib import Path
+import json
+
+# Read data as a string and convert to a Python object.
+BASE = Path(__file__).parent
+path = BASE/"eq_data/eq_data_1_day_m1.geojson"
+contents = path.read_text()
+# loads: str -> obj
+all_eq_data = json.loads(contents)
+
+# Create a more readable version of the data file.
+path = BASE/"eq_data/readable_eq_data.geojson"
+# dumps: obj -> str
+readable_contents = json.dumps(all_eq_data, indent=4)
+path.write_text(readable_contents)
